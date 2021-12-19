a3429833ce715e4d11926c86208b564627adcb7a
Vasil Radoykov <vradoyko@visteon.com>
2021-12-19 23:56:03 +0200

    Validation added

--- src/components/Taskboard/EditTaskModal.js
+++ src/components/Taskboard/EditTaskModal.js
@@ -17,17 +17,20 @@ const EditTaskModal = ({
         status: 1,
         tags: [1, 3]
     });
+    const [errors, setErrors] = useState({ title: false, description: false });
 
     useEffect(() => {
         if (taskId) {
             taskService.getTask(taskId)
                 .then(res => setTask(res))
         }
+        setErrors({ title: false, description: false });
     }, [taskId])
 
 
     const handleInputChange = (e) => {
         const { name, value } = e.target;
+        validateForm(e);
         setTask((t) => {
             return { ...t, [name]: value }
         });
@@ -42,7 +45,31 @@ const EditTaskModal = ({
             })
             .catch((err) => notifications.createError(`${err.code} - ${err.message}`));
         handleClose();
-        
+
+    };
+
+    const validateForm = (e) => {
+        const currentValue = e.target.value;
+        switch (e.target.name) {
+            case 'title':
+                if (currentValue.length < 3) {
+                    setErrors(state => ({ ...state, title: 'Title sould be at least 3 characters!' }))
+                } else {
+                    setErrors(state => ({ ...state, title: false }))
+                }
+                break;
+            case 'description':
+                if (currentValue.length < 3) {
+                    setErrors(state => ({ ...state, description: 'Description sould be at least 3 characters!' }))
+                } else {
+                    setErrors(state => ({ ...state, description: false }))
+                }
+                break;
+
+            default:
+                setErrors(state => ({ ...state, title: false, description: false }));
+                break;
+        }
     };
 
     return (
@@ -60,11 +87,13 @@ const EditTaskModal = ({
                     <Form.Group className="mb-3" controlId="formGridAddress1">
                         <Form.Label>Name</Form.Label>
                         <Form.Control name="title" placeholder="Enter task name..." value={task.title} onChange={handleInputChange} />
+                        {errors.title && <span className="validation-error">{errors.title}</span>}
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="formGridAddress2">
                         <Form.Label>Description</Form.Label>
                         <Form.Control as="textarea" name="description" rows={3} placeholder="Enter short description..." value={task.description} onChange={handleInputChange} />
+                        {errors.description && <span className="validation-error">{errors.description}</span>}
                     </Form.Group>
 
                     <Row className="mb-3">
@@ -88,7 +117,7 @@ const EditTaskModal = ({
                         </Form.Group>
                     </Row>
                     <div className="d-flex justify-content-end align-items-center">
-                        <Button variant="primary" className="mx-2" onClick={handleSubmit}>Save</Button>
+                        <Button variant="primary" className="mx-2" onClick={handleSubmit} disabled={errors.title || errors.description}>Save</Button>
                         <Button variant="secondary" onClick={handleClose}>Close</Button>
                     </div>
 
